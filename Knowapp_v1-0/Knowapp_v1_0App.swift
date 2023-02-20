b9276a3eafdcec9a3d0d07bfb44507a21fb0a5c2
Omni-star <alexcrov99@gmail.com>
2023-02-20 12:50:35 +0100

    Initial Commit

---
+++ Knowapp_v1-0/Knowapp_v1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Knowapp_v1_0App.swift
+//  Knowapp_v1-0
+//
+//  Created by alessiocrovella on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Knowapp_v1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
