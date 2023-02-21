b14674d8f61ce71b5ffd464fe88fdb75802d4c31
Omni-star <alexcrov99@gmail.com>
2023-02-21 23:48:01 +0100

    tab bar

---
+++ Knowapp_v1-0/TabSwiftUIView.swift
@@ -0,0 +1,45 @@
+//
+//  TabSwiftUIView.swift
+//  Knowapp_v1-0
+//
+//  Created by alessiocrovella on 21/02/23.
+//
+
+import SwiftUI
+
+struct TabSwiftUIView: View {
+    
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor(Color("Color"))
+        UITabBar.appearance().unselectedItemTintColor = UIColor(Color("UnselectedItem"))
+    }
+    
+    var body: some View {
+        TabView {
+            VStack {
+                EmptyView()
+            }
+            .tabItem {
+                Label("Categories", systemImage: "book")
+            }
+            VStack {
+                EmptyView()
+            }
+            .tabItem {
+                Label("Q&A", systemImage: "message.fill")
+            }
+            VStack {
+                EmptyView()
+            }
+            .tabItem {
+                Label("Search", systemImage: "magnifyingglass")
+            }
+        }.tint(Color("AccentColor"))
+    }
+}
+
+struct TabSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabSwiftUIView()
+    }
+}
