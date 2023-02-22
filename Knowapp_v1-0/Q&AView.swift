b3d4c432460cd7aae0efda929490bc09ddc06ea8
Omni-star <alexcrov99@gmail.com>
2023-02-22 13:01:44 +0100

    Q&A sketches

---
+++ Knowapp_v1-0/Q&AView.swift
@@ -0,0 +1,65 @@
+//
+//  Q&AView.swift
+//  Knowapp_v1-0
+//
+//  Created by alessiocrovella on 22/02/23.
+//
+
+import SwiftUI
+
+struct Q_AView: View
+{
+    @State private var selected = 0
+    var sezioni = ["QUESTIONS", "ADD QUESTIONS"]
+    var body: some View
+    {
+            VStack
+            {
+                Text("Q&A")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                SegmentedView(captions: sezioni, selected: $selected)
+                if selected == 0
+                {
+                    ZStack{
+                        QuestionsView(searchText: "")
+                    }
+                } else {
+                    ZStack{
+                        Image("Q_ABackground")
+                            .resizable()
+                            .ignoresSafeArea()
+                        Text("weeeeeeeeeeeeeeeee")
+                    }
+                }
+                Spacer()
+            }
+    }
+}
+struct SegmentedView: View
+{
+    var captions = Array<String>()
+    @Binding var selected: Int
+
+    var body: some View
+    {
+        VStack
+        {
+            Picker(selection: $selected, label: Text(""))
+            {
+                ForEach(0..<captions.count) { index in
+                    Text(self.captions[index]).tag(index)
+                }
+            }.pickerStyle(SegmentedPickerStyle())
+        }
+    }
+}
+
+
+struct Q_AViewView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        Q_AView()
+    }
+}
---
+++ Knowapp_v1-0/QuestionsView.swift
@@ -0,0 +1,70 @@
+//
+//  QuestionsView.swift
+//  Knowapp_v1-0
+//
+//  Created by alessiocrovella on 22/02/23.
+//
+
+import SwiftUI
+
+struct QuestionsView: View
+{
+    
+    @State var searchText: String
+    var films = ["Episodio1","Episodio2","Episodio"]
+    
+    var body: some View
+    {
+            NavigationStack
+            {
+                    List
+                    {
+                        ForEach(filtered, id:\.self)
+                        {
+                            film in ZStack{
+                                NavigationLink (destination: EmptyView())
+                                {
+                                    EmptyView()
+                                }.opacity(0)
+                                VStack
+                                {
+                                    Text(film)
+                                    Image("stanford").resizable().scaledToFit()
+                                }
+                            }
+                        }
+                    }.background(Image("Q_ABackground"))
+                    .scrollContentBackground(.hidden)
+            }.searchable(text: $searchText)
+    }
+
+    //    variabile calcolata al momento
+    var filtered: [String]
+    {
+        if searchText.isEmpty
+        {
+            return films
+        } else
+        {
+            return films.filter({(nome: String)
+                -> Bool in
+                if nome.lowercased().contains(searchText.lowercased())
+                {
+                    return true
+                }
+                else
+                {
+                    return false
+                }
+            })
+        }
+    }
+}
+
+struct QuestionsView_Previews: PreviewProvider
+{
+    static var previews: some View
+{
+        QuestionsView(searchText: "")
+    }
+}
